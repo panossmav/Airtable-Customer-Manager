984948edc183e4bae26ef72b490831b52dcd4444
Panos <panossmav@icloud.com>
2025-07-13 23:40:09 +0300

    Add user login / logs

--- funcs.py
+++ funcs.py
@@ -3,6 +3,10 @@ import pyairtable
 import os
 import dotenv
 from pyairtable import Api
+import hashlib
+import pytz
+
+gr_time = datetime.now(pytz.timezone("Europe/Athens"))
 
 dotenv.load_dotenv()
 
@@ -15,5 +19,24 @@ products_table = api.table(base_id,"Products")
 orders_table = api.table(base_id,"Orders")
 users_table = api.table(base_id,"App users")
 logs_table = api.table(base_id,"User Logs")
+
+
+def check_user_pass(u, p):
+    p_e = hashlib.sha256(p.encode()).hexdigest()
+    formula = f"{{Username}} = '{u}'"
+    usern = users_table.all(formula=formula, fields=["Password"])
+    if usern:
+        password = usern[0]["fields"].get("Password")
+        if password == p_e:
+            return True  # Auth success
+        else:
+            return False  # Wrong password
+    else:
+        return False  # No such user
+    
+
+
+def create_user_logs(u,act):
+    logs_table.create({"User":u,"Action":act,"Date / Time":gr_time.isoformat(sep=' ', timespec='seconds')})
 
 
--- main_app.py
+++ main_app.py
@@ -1,9 +1,38 @@
 from tkinter import *
 import tkinter as tk
+from funcs import *
 
 app=tk.Tk()
 app.geometry('500x500')
 app.title('Airtable Customer / Order Manager')
 
+tk.Label(app,text='Username: ').pack()
+username_e = Entry(app)
+username_e.pack()
+tk.Label(app,text='Password: ').pack()
+passw_e = Entry(app,show='•')
+passw_e.pack()
 
+
+def clear_app():
+    for widget in app.winfo_children():
+        widget.pack_forget()
+
+def home():
+    clear_app()
+    tk.Label(app,text='Καλωσορίσες %s. \n Επίλεξε μια ενέργεια'%username).pack()
+
+def login_click():
+    global username
+    username = username_e.get()
+    passw = passw_e.get()
+    auth = check_user_pass(username,passw)
+    if auth == True:
+        create_user_logs(username,'Logged in.')
+        clear_app()
+        home()
+    else:
+        var = tk.StringVar(value='Λάθος συνδιασμός. Δοκιμάστε ξανά')
+        tk.Label(app,textvariable=var).pack()
+tk.Button(app,text='Σύνδεση',command=login_click).pack()
 app.mainloop()
